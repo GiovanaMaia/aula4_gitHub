75dfdb360fdf8c5be4ea726b8f76997705786090
GiovanaMaia <giovanamaia851@gmail.com>
2024-03-01 16:56:03 -0300

    Versao Develop
    Modifiquei o primeiro codigo colocando a idade

--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@ using namespace std;
 int main()
 {
     string nome = "Giovana";
-    std::cout<<nome<< endl;
+    int idade = 17;
+    cout<<nome<< endl;
+    cout<<idade<< endl;
 
     return 0;
 }

f279734a016ebc0d5798d27d7f94c39a7d0f6bac
omeranha <53233626+omeranha@users.noreply.github.com>
2024-03-01 21:31:10 -0300

    colocando nomecolega

--- main.cpp
+++ main.cpp
@@ -4,9 +4,11 @@ using namespace std;
 int main()
 {
     string nome = "Giovana";
+    string nomeDoColega = "Emanuel";
     int idade = 17;
     cout<<nome<< endl;
     cout<<idade<< endl;
+    cout << nomeDoColega << endl;
 
     return 0;
 }
